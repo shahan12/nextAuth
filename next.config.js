ff2bceec23c57ae078cc770b1e74de7bd74a9fae
shahan12 <shahankhan485@gmail.com>
2023-05-21 18:43:16 +0530

    secrets added

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
+//"827785021523-bgp04ure3biobim2n8rmlbfqkitekb5g.apps.googleusercontent.com",
+// "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"
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
   env: {
-    GOOGLE_CLIENT_ID:
-      "827785021523-bgp04ure3biobim2n8rmlbfqkitekb5g.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
 };
 
