2f9845616af4d301fe32fe920608e3bb55929c23
shahan12 <shahankhan485@gmail.com>
2023-05-23 00:50:45 +0530

    side nav fix

--- public/component/sideNav/sideNav.jsx
+++ public/component/sideNav/sideNav.jsx
@@ -1,15 +1,38 @@
 "use client";
+import { useState } from "react";
 import "./SideNav.css";
+import Dashboard from "../../../public/dashboard_icondashbord.png";
+import Transaction from "../../../public/transaction_icontransaction.png";
+import Schedule from "../../../public/schedule_icon.png";
+import User from "../../../public/user_icon.png";
+import Setting from "../../../public/setting_icon.png";
+import Image from "next/image";
 
 const SideNav = () => {
   const navTitle = [
-    "Dashboard",
-    "Transaction",
-    "Schedule",
-    "Users",
-    "Settings",
+    {
+      name: "Dashboard",
+      img: Dashboard,
+    },
+    {
+      name: "Transaction",
+      img: Transaction,
+    },
+    {
+      name: "Schedule",
+      img: Schedule,
+    },
+    {
+      name: "Users",
+      img: User,
+    },
+    {
+      name: "Settings",
+      img: Setting,
+    },
   ];
   const footerText = ["Help", "Contact Us"];
+  const [activeTab, setActiveTab] = useState("Dashboard");
 
   return (
     <div className="sideNavWrapper">
@@ -18,9 +41,20 @@ const SideNav = () => {
         <div className="section-2">
           <ol className="nav-options">
             {navTitle.map((item, index) => (
-              <l1 className="nav-title" key={index}>
-                {item}
-              </l1>
+              <div className="nav-options-cotainer">
+                <div className="side-nav-img">
+                  <Image src={item.img} className="img-nav" />
+                </div>
+                <l1
+                  className={`nav-title ${
+                    activeTab === item.name && "add-active-style"
+                  }`}
+                  key={index}
+                  onClick={() => setActiveTab(item.name)}
+                >
+                  {item.name}
+                </l1>
+              </div>
             ))}
           </ol>
         </div>
