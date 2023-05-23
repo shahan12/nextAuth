33716c42886c3f0c1d5bba5c2213b911de95b695
shahan12 <shahankhan485@gmail.com>
2023-05-23 15:11:55 +0530

    env changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 //process.env.GOOGLE_CLIENT_ID :"827785021523-bgp04ure3biobim2n8rmlbfqkitekb5g.apps.googleusercontent.com",
 // process.env.GOOGLE_CLIENT_SECRET :"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"
 // process.env.NEXTAUTH_SECRET :shahanProject
+//NEXTAUTH_URL: http://localhost:3000/
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
