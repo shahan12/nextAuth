cff00a0d46ead8ff1a56b7979e015133b8e6b3e0
shahan12 <shahankhan485@gmail.com>
2023-05-23 15:50:58 +0530

    env changes

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    // NEXTAUTH_URL: "http://localhost:3000/",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
