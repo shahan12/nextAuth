374d1a969bec96ced873a21c94ea08195e9ead7c
shahan12 <shahankhan485@gmail.com>
2023-05-23 14:18:40 +0530

    env file changes

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
-//"827785021523-bgp04ure3biobim2n8rmlbfqkitekb5g.apps.googleusercontent.com",
-// "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"
+//process.env.GOOGLE_CLIENT_ID :"827785021523-bgp04ure3biobim2n8rmlbfqkitekb5g.apps.googleusercontent.com",
+// process.env.GOOGLE_CLIENT_SECRET :"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"
+// process.env.NEXTAUTH_SECRET :shahanProject
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
   env: {
-    DB_URI: "mongodb://localhost:27017/next13-auth",
-    NEXTAUTH_SECRET: "shahanProject",
-    NEXTAUTH_URL: "http://localhost:3000/",
-    GOOGLE_CLIENT_ID:
-      "827785021523-bgp04ure3biobim2n8rmlbfqkitekb5g.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    // NEXTAUTH_URL: "http://localhost:3000/",
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
 };
 
